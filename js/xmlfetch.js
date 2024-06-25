9a7591f6492c5016d3eb9109a428ef1f866c1f9d
Hamza Bin Mazhar <hmazhar@pulsarmeasurement.com>
2024-06-25 11:45:43 +0100

    Test 3 for fetch, change fetch path to include root repo.

--- js/xmlfetch.js
+++ js/xmlfetch.js
@@ -1,4 +1,4 @@
-fetch("xml/reflect-e_0.1.7_default.xml")
+fetch("HybridWebApp/xml/reflect-e_0.1.7_default.xml")
   .then((response) => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
